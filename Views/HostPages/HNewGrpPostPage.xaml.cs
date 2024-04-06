66178b13b7ae4786904a23dbc9180c5951f51a35
Ali Hamza <alihamza.bcss19@iba-suk.edu.pk>
2024-04-06 17:57:30 +0500

    Again login navigates to HHome and in NewGrpPost back nav added

--- Views/HostPages/HNewGrpPostPage.xaml.cs
+++ Views/HostPages/HNewGrpPostPage.xaml.cs
@@ -13,7 +13,7 @@ public partial class HNewGrpPostPage : ContentPage
 
     private void btnBack_Click(object sender, EventArgs e)
     {
-
+        Shell.Current.GoToAsync(nameof(HPOSTURSentMsgsPage));
     }
 
     private void sendButton_Tapped(object sender, TappedEventArgs e)
